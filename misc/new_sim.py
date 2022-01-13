6b40057ad74dc008973b2150083eee33dd62ea7f
katie-lim <35044591+katie-lim@users.noreply.github.com>
2022-01-13 12:01:46 +0000

    automatically generate planet .cfg files

--- misc/new_sim.py
+++ misc/new_sim.py
@@ -1,32 +1,82 @@
 import sys
+from shutil import copyfile
 import re
 
 setupName = sys.argv[1]
 
-regex = "([\d.]+j_[\d.]+j)_([\d.]+)s_(\d)a_([\d.]+)pe"
-matches = re.findall(regex, setupName)
-print(matches[0])
+def generate_planet_cfg(planetNames):
+    planet_mass_multipliers = [float(planet[:-1]) for planet in planetNames.split("_")]
 
-if len(matches) > 0:
-    planet_cfg, surface_density, alpha, photoevaporation = matches[0]
+    planet_masses = [0.001 * pm for pm in planet_mass_multipliers]
 
-    surf_dens = float(surface_density)
-    pe = float(photoevaporation)
+    # Generate the .cfg file
+    fileName = ("planets/%s.cfg" % planetNames)
+    copyfile("planets/planet_template.cfg", fileName)
+
+    with open(fileName, "a") as f:
+        # By default, start the 1st planet at r=1,
+        # the 2nd at r=2, and the 3rd at r=3, etc
+
+        for i in range(len(planet_masses)):
+            f.write("\nJupiter%d	 	%.2f		%s	 0.0		YES   		YES" % (i+1, float(i+1), planet_masses[i]))
 
 
-    # Create the .par file for the simulation
-    f1 = open('setups/fargo/template.par', 'r')
-    f2 = open("setups/fargo/%s.par" % setupName, 'w')
+    # Generate the .cfg file with fixed planets (set 'Feels disk' and 'Feels others' to NO)
+    fileName = ("planets/fixed/%s_fixed.cfg" % planetNames)
+    copyfile("planets/planet_template.cfg", fileName)
 
-    placeholders = ("{{SIGMA0}}", "{{ALPHA}}", "{{PHOTOEVAPORATION}}", "{{PLANET_CFG}}", "{{OUTPUT}}")
-    replace = ("%.10e" % (6.3661977237e-4 * surf_dens), alpha, "%.2e" % (4.0e43 * (pe**2)), planet_cfg, setupName)
+    with open(fileName, "a") as f:
+        for i in range(len(planet_masses)):
+            f.write("\nJupiter%d	 	%.2f		%s	 0.0		NO   		NO" % (i+1, float(i+1), planet_masses[i]))
 
-    for line in f1:
-        for placeholder, rep in zip(placeholders, replace):
-            line = line.replace(placeholder, rep)
-        f2.write(line)
 
-    f1.close()
-    f2.close()
-else:
-    print("The simulation label %s doesn't follow convention." % setupName)
+
+def generate_par_file(setupName):
+    regex = "([\d.]+j_[\d.]+j)_([\d.]+)s_(\d)a_([\d.]+)pe"
+    matches = re.findall(regex, setupName)
+    print(matches[0])
+
+    if len(matches) > 0:
+        planetNames, surfaceDensity, alpha, photoevaporation = matches[0]
+
+        planet_cfg = "planets/%s.cfg" % planetNames
+        planet_cfg_fixed = "planets/fixed/%s_fixed.cfg" % planetNames
+        surf_dens = float(surfaceDensity)
+        pe = float(photoevaporation)
+
+        # Generate the planet .cfg files
+        generate_planet_cfg(planetNames)
+
+
+        # Create the .par files for the simulation
+        f1 = open('setups/fargo/template.par', 'r')
+        f2 = open("setups/fargo/%s.par" % setupName, 'w')
+        f3 = open("setups/fargo/fixed/%s_fixed.par" % setupName, 'w')
+
+        placeholders = ("{{SIGMA0}}", "{{ALPHA}}", "{{PHOTOEVAPORATION}}", "{{PLANET_CFG}}", "{{OUTPUT}}")
+        replace = ("%.10e" % (6.3661977237e-4 * surf_dens), alpha, "%.2e" % (4.0e43 * (pe**2)), planet_cfg, setupName)
+
+        for line in f1:
+            for placeholder, rep in zip(placeholders, replace):
+                line = line.replace(placeholder, rep)
+            f2.write(line)
+
+
+            if (line[:12] == "PlanetConfig"):
+                line = line.replace(planet_cfg, planet_cfg_fixed)
+            elif (line[:9] == "FixedNtot"):
+                line = line.replace("FixedNtot", "Ntot")
+            elif (line[:4] == "Ntot"):
+                line = line.replace("Ntot", "TotalNtot")
+
+            f3.write(line)
+
+        f1.close()
+        f2.close()
+        f3.close()
+
+    else:
+        print("The simulation label %s doesn't follow convention." % setupName)
+
+
+generate_par_file(setupName)
