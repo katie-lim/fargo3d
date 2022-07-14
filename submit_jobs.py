2eabe72df04f13a3163fbf7bfa76609f37d9ce2e
katie-lim <35044591+katie-lim@users.noreply.github.com>
2022-07-14 11:04:20 +0100

    add new simulations with higher alpha and surface density

--- submit_jobs.py
+++ submit_jobs.py
@@ -43,14 +43,17 @@ def getRunningSims():
 
 simsToSubmit = []
 sims = [
-        "1j_1j_3s_3a_0pe",
-        "1j_1j_3s_4a_0pe",
-        "5j_3j_6s_4a_0pe",
-        "3j_5j_6s_4a_0pe",
-        "1.33j_1j_2s_3a_0pe",
-        "1j_1.33j_2s_3a_0pe",
-        "0.7j_0.7j_1.4s_3a_0pe",
-        "0.7j_0.7j_1.4s_4a_0pe",
+        "1j_1j_1s_1a_0pe",
+        "1.33j_1j_1s_1a_0pe",
+        "1j_1.33j_1s_1a_0pe",
+        "2j_2j_1s_1a_0pe",
+        "3j_3j_1s_1a_0pe",
+        "2j_3j_1s_1a_0pe",
+        "3j_2j_1s_1a_0pe",
+        "5j_5j_1s_1a_0pe",
+        "3j_5j_1s_1a_0pe",
+        "5j_3j_1s_1a_0pe",
+        "0.7j_0.7j_1s_1a_0pe",
         ]
 
 runningSims = getRunningSims()
