13fe3dd6e6660339344370ca0494841910218941
katie-lim <35044591+katie-lim@users.noreply.github.com>
2022-07-15 15:04:29 +0100

    add simulations with lower surf dens and higher aspect ratio

--- submit_jobs.py
+++ submit_jobs.py
@@ -54,6 +54,18 @@ sims = [
         "3j_5j_1s_1a_7h_0pe",
         "5j_3j_1s_1a_7h_0pe",
         "0.7j_0.7j_1s_1a_7h_0pe",
+
+        "1j_1j_0.667s_1a_9h_0pe",
+        "1.33j_1j_0.667s_1a_9h_0pe",
+        "1j_1.33j_0.667s_1a_9h_0pe",
+        "2j_2j_0.667s_1a_9h_0pe",
+        "3j_3j_0.667s_1a_9h_0pe",
+        "2j_3j_0.667s_1a_9h_0pe",
+        "3j_2j_0.667s_1a_9h_0pe",
+        "5j_5j_0.667s_1a_9h_0pe",
+        "3j_5j_0.667s_1a_9h_0pe",
+        "5j_3j_0.667s_1a_9h_0pe",
+        "0.7j_0.7j_0.667s_1a_9h_0pe",
         ]
 
 runningSims = getRunningSims()
