9e73771b6ebabf5dde64e60aa2999b5aefc55362
katie-lim <35044591+katie-lim@users.noreply.github.com>
2022-08-18 00:47:32 +0100

    add code to plot angular velocities for all simulations

---
+++ plot_all_orbital_velocities.py
@@ -0,0 +1,14 @@
+from analysis.plot import *
+from analysis.utilities import *
+from analysis.plot_results import getListOfSimulationOutputs
+
+for setupName in getListOfSimulationOutputs():
+
+    Nframes = findLastOutputNumber(setupName)
+
+    velocity = "outputs/%s/gasvx%d.dat" % (setupName, Nframes)
+    surfdens = "outputs/%s/gasdens%d.dat" % (setupName, Nframes)
+    parFile = "setups/fargo/%s.par" % (setupName)
+    saveFileName = "analysis/plots/angular_velocity/angular_velocity_%s.png" % (setupName)
+
+    plotAzimuthallyAvgedOrbitalVel([velocity], [surfdens], [parFile], logRadialSpacing=True, massWeighted=True, angularVelocity=True, title=velocity, saveFileName=saveFileName)
--- plot_orbital_velocities.py
+++ plot_orbital_velocities.py
@@ -5,16 +5,15 @@ from analysis.utilities import *
 import sys
 setupName = sys.argv[1]
 
-setupName = "10j_10j_0.2667s_5a_6h_0pe"
-
+# setupName = "10j_10j_0.2667s_5a_6h_0pe"
 Nframes = findLastOutputNumber(setupName)
 # Nframes = 0
 
 velocity = "outputs/%s/gasvx%d.dat" % (setupName, Nframes)
 surfdens = "outputs/%s/gasdens%d.dat" % (setupName, Nframes)
 parFile = "setups/fargo/%s.par" % (setupName)
+saveFileName = "analysis/plots/angular_velocity/angular_velocity_%s.png" % (setupName)
 
-
-plotAzimuthallyAvgedOrbitalVel([velocity], [surfdens], [parFile], logRadialSpacing=True, massWeighted=True, title=velocity)
-plotAzimuthallyAvgedOrbitalVel([velocity], [surfdens], [parFile], logRadialSpacing=True, massWeighted=True, angularVelocity=True, title=velocity)
+# plotAzimuthallyAvgedOrbitalVel([velocity], [surfdens], [parFile], logRadialSpacing=True, massWeighted=True, title=velocity)
+plotAzimuthallyAvgedOrbitalVel([velocity], [surfdens], [parFile], logRadialSpacing=True, massWeighted=True, angularVelocity=True, title=velocity, saveFileName=saveFileName)
 # %%
