5045e5f2460aa41c608e6ae4981cbe475e5296ea
katie-lim <35044591+katie-lim@users.noreply.github.com>
2022-08-18 11:11:00 +0100

    add orbital velocity plots

--- plot_all_orbital_velocities.py
+++ plot_all_orbital_velocities.py
@@ -3,12 +3,17 @@ from analysis.utilities import *
 from analysis.plot_results import getListOfSimulationOutputs
 
 for setupName in getListOfSimulationOutputs():
+    print(setupName)
+    try:
+        Nframes = findLastOutputNumber(setupName)
 
-    Nframes = findLastOutputNumber(setupName)
+        velocity = "outputs/%s/gasvx%d.dat" % (setupName, Nframes)
+        surfdens = "outputs/%s/gasdens%d.dat" % (setupName, Nframes)
+        parFile = "setups/fargo/%s.par" % (setupName)
+        saveFileName = "analysis/plots/angular_velocity/angular_velocity_%s.png" % (setupName)
 
-    velocity = "outputs/%s/gasvx%d.dat" % (setupName, Nframes)
-    surfdens = "outputs/%s/gasdens%d.dat" % (setupName, Nframes)
-    parFile = "setups/fargo/%s.par" % (setupName)
-    saveFileName = "analysis/plots/angular_velocity/angular_velocity_%s.png" % (setupName)
+        plotAzimuthallyAvgedOrbitalVel([velocity], [surfdens], [parFile], logRadialSpacing=True, massWeighted=True, angularVelocity=True, title=velocity, saveFileName=saveFileName)
 
-    plotAzimuthallyAvgedOrbitalVel([velocity], [surfdens], [parFile], logRadialSpacing=True, massWeighted=True, angularVelocity=True, title=velocity, saveFileName=saveFileName)
+    except:
+        print("Failed.")
+        continue
